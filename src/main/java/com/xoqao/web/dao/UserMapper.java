be2eb9c20402b59909a83a34ef7b9637e34dfd58
Lovegod <dx96_j@163.com>
2017-07-09 11:52:40 +0800

    基本环境测试，实现用户查看demo

--- src/main/java/com/xoqao/web/dao/UserMapper.java
+++ src/main/java/com/xoqao/web/dao/UserMapper.java
@@ -1,34 +1,17 @@
 package com.xoqao.web.dao;
 
 
-import com.xoqao.web.bean.User;
-import com.xoqao.web.bean.UserExample;
+import com.xoqao.web.bean.user.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
 @Service
 public interface UserMapper {
-    int countByExample(UserExample example);
 
-    int deleteByExample(UserExample example);
+    User findUserByphone(@Param("phone") String phone) throws Exception;
 
-    int deleteByPrimaryKey(Integer uid);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    List<User> selectByExample(UserExample example);
-
-    User selectByPrimaryKey(Integer uid);
-
-    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
+    List<User> findAllUsers()throws Exception;
 }
